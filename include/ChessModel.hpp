abfbc2b78f7df891508fe3a71652bfb1c54df1b0
Hlebushkek <volsoor@gmail.com>
2023-08-04 01:36:45 -0700

    Add ChessBoardPieceObject and ChessModel +  refactor ChessBoard

---
+++ include/ChessModel.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <array>
+#include "ChessTypes.hpp"
+#include "ChessPieces.hpp"
+
+class ChessModel
+{
+public:
+    ChessModel();
+    ~ChessModel() = default;
+
+    std::vector<std::pair<glm::ivec2, MoveType>> GetSelectionFor(ChessPiece *piece);
+
+    ChessPiece *GetPieceAt(glm::ivec2 position);
+    ChessPiece *GetPieceAt(int x, int y);
+
+    bool PositionExists(glm::ivec2 position);
+    bool PositionExists(int x, int y);
+
+    glm::vec2 GetPositionFor(ChessPiece *piece);
+private:
+    std::array<std::array<ChessPiece*, 8>, 8> boardState;
+
+};
